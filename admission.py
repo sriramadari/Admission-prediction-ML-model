429bd5093496f316cbfe544e47c54f9a61d5e33e
unknown <lakshmisriramadari1427@gmail.com>
2023-04-10 21:58:15 +0530

    logistic regression model

---
+++ admission.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+import copy
+import math
+# load dataset
+X, y = load_data("data/ex2data1.txt")
+# Plot examples
+plot_data(X, y[:], pos_label="Admitted", neg_label="Not admitted")
+
+# Set the y-axis label
+plt.ylabel('Exam 2 score') 
+# Set the x-axis label
+plt.xlabel('Exam 1 score') 
+plt.legend(loc="upper right")
+plt.show()
+#sigmoid function
+def sigmoid(z):
+    g = 1/(1+np.exp(-z))  
+    return g
+def compute_cost(X, y, w, b, *argv):
+    m, n = X.shape
+    cost=0.0
+    for i in range(m):
+        z_i=np.dot(X[i],w)+b
+        f_wb_i=sigmoid(z_i)
+        cost+=-y[i]*np.log(f_wb_i)-(1-y[i])*np.log(1-f_wb_i)
+    total_cost=cost/m
+    ### END CODE HERE ### 
+
+    return total_cost
+
+def compute_gradient(X, y, w, b, *argv): 
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+
+    ### START CODE HERE ### 
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m       
+    ### END CODE HERE ###
+
+        
+    return dj_db, dj_dw
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters, lambda_): 
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w_history = []
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient(X, y, w_in, b_in, lambda_)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w_in = w_in - alpha * dj_dw               
+        b_in = b_in - alpha * dj_db              
+       
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  compute_cost(X, y, w_in, b_in, lambda_)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0 or i == (num_iters-1):
+            w_history.append(w_in)
+            #print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w_in, b_in, J_history, w_history #return w and J,w history for graphing
+np.random.seed(1)
+initial_w = 0.01 * (np.random.rand(2) - 0.5)
+initial_b = -8
+
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.001
+
+w,b, J_history,_ = gradient_descent(X ,y, initial_w, initial_b,compute_cost, compute_gradient, alpha, iterations, 0)
+
+def predict(X, w, b): 
+    # number of training examples
+    m, n = X.shape   
+    p = np.zeros(m)
+   
+    ### START CODE HERE ### 
+    # Loop over each example
+    for i in range(m):   
+        z_wb =0
+        # Loop over each feature
+        for j in range(n): 
+            # Add the corresponding term to z_wb
+            z_wb+= X[i,j]*w[j]
+        # Add bias term 
+        z_wb += b
+        
+        # Calculate the prediction for this example
+        f_wb = sigmoid(z_wb)
+
+        # Apply the threshold
+        p[i] = f_wb>=0.5
+        
+    ### END CODE HERE ### 
+    return p
